e7a31591fdde0203735670ba614b2ccf7c3faa87
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-10-22 20:27:59 +0300

    Setup Frontend User

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 Route::get('/dashboard', [HomeController::class, 'Premium'])->name('dashboard')->middleware(['auth', 'verified']);
