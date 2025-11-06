291104eb63414aaefb015833ee8384e066a7ceb6
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-11-06 20:11:11 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -21,14 +22,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    //Admin Route
+
+    //Admin Categories Page
     Route::get('/read_category', [CategoryController::class, 'read_category'])->name('read_category');
     Route::get('/add_category', [CategoryController::class, 'add_category'])->name('add_category');
     Route::post('/create_category', [CategoryController::class, 'create_category'])->name('create_category');
     Route::get('/edit_category/{id}', [CategoryController::class, 'edit_category'])->name('edit_category');
     Route::post('/update_category/{id}', [CategoryController::class, 'update_category'])->name('update_category');
     Route::get('/delete_category/{id}', [CategoryController::class, 'delete_category'])->name('delete_category');
+
+        //Admin Products Page
+    Route::get('/read_product', [ProductController::class, 'read_product'])->name('read_product');
+    Route::get('/add_product', [ProductController::class, 'add_product'])->name('add_product');
+    Route::post('/create_product', [ProductController::class, 'create_product'])->name('create_product');
+    Route::get('/edit_product/{id}', [ProductController::class, 'edit_product'])->name('edit_product');
+    Route::post('/update_product/{id}', [ProductController::class, 'update_product'])->name('update_product');
+    Route::get('/delete_product/{id}', [ProductController::class, 'delete_product'])->name('delete_product');
+
 });
 
 require __DIR__.'/auth.php';
